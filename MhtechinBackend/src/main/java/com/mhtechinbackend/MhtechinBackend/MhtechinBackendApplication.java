fa462cdb717256f90f9bc672ddfea5ef86cd23fc
parag1627 <paragpatil973047@gmail.com>
2023-08-03 16:44:21 +0530

    first commit by parag

---
+++ MhtechinBackend/src/main/java/com/mhtechinbackend/MhtechinBackend/MhtechinBackendApplication.java
@@ -0,0 +1,13 @@
+package com.mhtechinbackend.MhtechinBackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MhtechinBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MhtechinBackendApplication.class, args);
+	}
+
+}
---
+++ MhtechinBackend/src/test/java/com/mhtechinbackend/MhtechinBackend/MhtechinBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mhtechinbackend.MhtechinBackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MhtechinBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
